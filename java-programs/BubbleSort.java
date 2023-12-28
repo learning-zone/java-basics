b9b06afc2c6014e8cbff007992d34216c7e3ea3f
NischalKothari <kotharinischal1@gmail.com>
2023-12-28 17:42:48 +0530

    More Efficient Bubble Sort Code

--- java-programs/BubbleSort.java
+++ java-programs/BubbleSort.java
@@ -7,15 +7,21 @@ package misc;
 public class BubbleSort {
 
 	public void bubbleSort(int[] arr) {
+		boolean swap;
 		int n = arr.length;
 		int tmp = 0;
 		for (int i = 0; i < n ; i++) {
-			for (int j = 1; j < n-1; j++) {
+			swap = false;
+			for (int j = 1; j < n-i; j++) {
 				if (arr[j-1] > arr[j]) {
-					tmp = arr[j - 1];
-					arr[j-1] = arr[j];
-					arr[j] = tmp;
+					tmp = arr[j];
+					arr[j] = arr[j-1];
+					arr[j-1] = tmp;
+					swap = true;
 				}
+			}
+			if (!swap){
+				break;
 			}
 		}
 	}
