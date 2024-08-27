968a729f2577549397f7ad75f0f3bb5ef79bf1c4
Kaustubh Pandey <146510603+kautubh01@users.noreply.github.com>
2024-08-27 22:42:13 +0530

    Update EnumConstructorExample.java

--- core-java/basics/EnumConstructorExample.java
+++ core-java/basics/EnumConstructorExample.java
@@ -1,24 +1,21 @@
-package basics;
-
 enum TrafficSignal {
 	RED("STOP"), GREEN("GO"), ORANGE("SLOW DOWN");
-	
-	private String action;
-	public String getAction() {
-		return this.action;
-	}
-	private TrafficSignal(String action) {
+
+	private final String action;
+
+	TrafficSignal(String action) {
 		this.action = action;
 	}
+
+	public String getAction() {
+		return action;
+	}
 }
+
 public class EnumConstructorExample {
-
 	public static void main(String[] args) {
-
-		TrafficSignal[] signals = TrafficSignal.values();
-		
-		for(TrafficSignal signal: signals){
-			System.out.println("name: "+ signal.name() + "  action: " + signal.getAction());
+		for (TrafficSignal signal : TrafficSignal.values()) {
+			System.out.println("name: " + signal.name() + "  action: " + signal.getAction());
 		}
 	}
 }
